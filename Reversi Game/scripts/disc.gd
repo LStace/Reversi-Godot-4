6a6dbf22aa5e770a20c990b0724d3039ae20e98f
Lee Stacey <lstacey048@gmail.com>
2024-02-25 13:16:55 +0000

    Added Disc Animations
    Created animation for flipping a disc from light to dark and from dark to light.

--- Reversi Game/scripts/disc.gd
+++ Reversi Game/scripts/disc.gd
@@ -1,10 +1,12 @@
 extends AnimatedSprite2D
 
-var animations = ["light_resting", "dark_resting"]
+var animations = [["light_resting", "dark_light_flip"], ["dark_resting", "light_dark_flip"]]
 
 
 func place(side):
-	play(animations[int(side)])
+	play(animations[int(side)][0])
 
 func flip(side):
-	play(animations[int(side)])
+	play(animations[int(side)][1])
+	#play(animations[int(side)][0])
+	
